070f9e483efa51052e9cd6cd9512176ddcf0ac58
ChaitanyaMakkena <cmakkena@my365.bellevue.edu>
2024-05-04 10:01:34 +0530

    Week8 assignment

--- src/main/java/com/bookclub/service/GenericDao.java
+++ src/main/java/com/bookclub/service/GenericDao.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public interface GenericDao<E, K> {
     
-    List<E> list(); // Return a list of objects of type E.
+	List<E> list(); // Return a list of objects of type E.
+	
+    List<E> list(String  key); // Return a list of objects of type E.
    
     E find(K key); // Return an object of type E by type K value.
 }
